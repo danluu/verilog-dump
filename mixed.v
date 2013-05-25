1cfcc55f848b8b349d8c4ae8ee5be9b8cdd5f304
Dan Luu <danluu@gmail.com>
2013-05-25 18:20:15 -0400

    Add examples of mixing combinational logic with sequential logic

---
+++ mixed.v
@@ -0,0 +1,27 @@
+// example of mixing combinational and sequential logic
+
+module mixa (q, a, b, clk, resetb);
+   output q;
+   input  a, b, clk, resetb;
+   reg 	  q;
+
+   always @(posedge clk or negedge resetb) begin
+      if (!resetb) q <= 1'b0;
+      else         q <= a ^ b;
+   end
+endmodule // mixa
+
+module mixb (q, a, b, clk, resetb);
+   output q;
+   input  a, b, clk, resetb;
+   reg 	  q, y;
+
+   always @(a or b) begin
+      y = a ^ b
+   end
+     
+   always @(clk or negedge resetb) begin
+      if (!resetb) q <= 1'b0;
+      else         q <= y;      
+   end
+endmodule
