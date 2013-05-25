018d8bffc67ddb2545d1bb7d6f62bfbd6bef35df
Dan Luu <danluu@gmail.com>
2013-05-25 16:16:30 -0400

    Add comment on full_case for decoder

--- decoder_example.v
+++ decoder_example.v
@@ -7,12 +7,15 @@ module decoder_example(ce0, ce1, cs, en, addr);
    // note: a casex here could cause a problem
    // e.g., if en = x, we'll match one of the cases
    // also, if high bit of address is X, we'll match into one of the ce cases
+
+   // note: if we were to use a full_case directive here,
+   // en would be optimized away and left as a dangling input
    always @ (addr or en) begin
       {ce0, ce1, cs} = 3'b0;
       casez ({addr, en})
 	3'b101: ce0 = 1'b1;
 	3'b111: ce1 = 1'b1;
-	3'b0?1: cs = 1'b1;
+	3'b0?1: cs  = 1'b1;
       endcase
    end
 endmodule
