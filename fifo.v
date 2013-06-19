a63db6bf7e6c8a7644fdf3ac0e0c0714680e5bcb
Dan Luu <danluu@gmail.com>
2013-06-18 19:43:04 -0500

    Temporarily remove SystemVerilog stuff that's erroring

--- fifo.v
+++ fifo.v
@@ -121,13 +121,13 @@ endmodule
 // ...
 // bin[n] = gray[n]                             // gray >> n
 
-module gray2bin #(parameter SIZE = 4)
-   (output logic [SIZE-1:0] bin,
-    input logic [SIZE-1:0] gray);
+//module gray2bin #(parameter SIZE = 4)
+//   (output logic [SIZE-1:0] bin,
+ //   input logic [SIZE-1:0] gray);
 
-   always_comb
-     for (int i=0; i < SIZE; i++)
-       bin[i] = ^(gray >> i);
-endmodule
+//   always_comb
+//     for (int i=0; i < SIZE; i++)
+//       bin[i] = ^(gray >> i);
+//endmodule
     
    
